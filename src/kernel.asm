1c26a4e02a05f7eba3f115b1b40dcc760c550493
rileeyyy <134024223+rileeyyy@users.noreply.github.com>
2024-05-11 21:01:15 -0400

    AAAAA

--- src/kernel.asm
+++ src/kernel.asm
@@ -26,48 +26,19 @@
     call print_string
 
 shell:
-    mov ah, 0
-    int 0x16
-    cmp al, 0x0D
-    jne not_enter
-    mov ah, 0x0e
-    mov al, 0x0a
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov si, prompt
-    call print_string
+    mov si, input_buffer
+    call read_string
 
-    mov si, buffer
-    mov di, command
-    cld
-    lodsb
-    mov bl, al
-    mov al, byte [di]
-    cmp al, bl
-    jne execute_command
-    inc di
-    mov si, di
-    mov di, buffer
-    call print_string
+    mov si, input_buffer
+    mov di, ver_command
+    call compare_strings
+    cmp al, 1
+    je print_version
     jmp shell
 
-execute_command:
-    mov si, buffer
-    mov di, command
-    cld
-    mov cx, 5
-    repe cmpsb
-    jne shell
-
-    mov si, buffer
-    add si, 5
+print_version:
+    mov si, version_string
     call print_string
-    jmp shell
-
-not_enter:
-    mov ah, 0x0e
-    int 0x10
     jmp shell
 
 print_string:
@@ -81,9 +52,36 @@ print_string:
 done_print:
     ret
 
+read_string:
+    mov ah, 0x0
+    int 0x16
+    mov ah, 0x0e
+    mov al, 0x0d
+    int 0x10
+    mov al, 0x0a
+    int 0x10
+    ret
+
+compare_strings:
+next_char:
+    lodsb
+    cmp al, byte [di]
+    jne not_equal
+    cmp al, 0
+    je equal
+    inc di
+    jmp next_char
+not_equal:
+    mov al, 0
+    ret
+equal:
+    mov al, 1
+    ret
+
 os_name db "epicOS v1.1", 0
 prompt db "> ", 0
-buffer db 100, 0
-command db "echo ", 0
+input_buffer db 32
+ver_command db "ver", 0
+version_string db "epicOS test build", 0
 
 times 512-($-$$) db 0
