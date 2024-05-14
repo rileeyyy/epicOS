e6676ce17ea2631373fdd5fdb05f955f0d94a316
rileeyyy <134024223+rileeyyy@users.noreply.github.com>
2024-05-13 21:18:32 -0400

    aaaaaaaa

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,13 +1,26 @@
 [org 0x8000]
-start:
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7c00
+    mov sp, 0x7000
+
+    mov ah, 0x06
+    xor al, al
+    mov bh, 0x07
+    xor cx, cx
+    mov dx, 0x184f
+    int 0x10
+
+    mov ah, 0x0e
+    mov al, 0x0a
+    int 0x10
 
     mov si, os_name
     call print_string
+    mov ah, 0x0e
+    mov al, 0x0a
+    int 0x10
 
     mov si, prompt
     call print_string
@@ -15,16 +28,15 @@ start:
 shell:
     mov ah, 0
     int 0x16
-
+    mov ah, 0x0e
+    int 0x10
     cmp al, 0x0D
     jne check_command
-
     mov ah, 0x0e
     mov al, 0x0a
     int 0x10
     mov al, 0x0d
     int 0x10
-
     mov si, prompt
     call print_string
     jmp shell
@@ -35,11 +47,12 @@ check_command:
     cld
     mov cx, 3
     repe cmpsb
-
     jne shell
+
     mov si, ver_msg
     call print_string
     jmp shell
+
 
 print_string:
     lodsb
@@ -58,7 +71,4 @@ buffer db 100, 0
 command db "ver", 0
 ver_msg db "epicOS test build", 0
 
-times 510-($-$$) db 0
-dw 0xaa55
-
-; radiohead
+times 512-($-$$) db 0
